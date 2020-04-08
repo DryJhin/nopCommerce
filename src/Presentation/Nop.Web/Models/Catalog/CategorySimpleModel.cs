756685e41e3d4da7113c1a8a9923ed1e286e3288
DryJhin <iosif2455@gmail.com>
2020-04-09 02:24:56 +0300

    changes to design

--- src/Presentation/Nop.Web/Models/Catalog/CategorySimpleModel.cs
+++ src/Presentation/Nop.Web/Models/Catalog/CategorySimpleModel.cs
@@ -23,5 +23,7 @@ namespace Nop.Web.Models.Catalog
         public bool HaveSubCategories { get; set; }
 
         public string Route { get; set; }
+
+        public bool IsSubCategory { get; set; }
     }
 }
