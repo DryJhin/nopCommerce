71c13c3a10f9f81b9cac4a7d420b8f197e164bad
DryJhin <iosif2455@gmail.com>
2020-04-15 08:20:26 +0300

    Added karma theme

---
+++ src/Presentation/Nop.Web/wwwroot/js/InitializeImageZoom.js
@@ -0,0 +1,8 @@
+﻿new Drift(document.querySelector('.drift-image'), {
+  paneContainer: document.querySelector('.overview'),
+  inlinePane: 900,
+  inlineOffsetY: -85,
+  containInline: true,
+  hoverBoundingBox: true,
+  zoomFactor: 4,
+});
