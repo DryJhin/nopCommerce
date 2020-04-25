1ec04da57b9aa96d880da9d692519eccb25ea3a4
DryJhin <iosif2455@gmail.com>
2020-04-25 17:31:42 +0300

    started plugin for blog removed serial key from foxnet solutions

---
+++ src/Nop.Plugin.Blog.Advanced/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nop.Plugin.Blog.Advanced
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Nop.Plugin.CodeStack.Blog.Advanced/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nop.Plugin.CodeStack.Blog.Advanced
+{
+    public class Class1
+    {
+    }
+}
